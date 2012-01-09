25a2060b818a5f4dc9e99e9229ce56ebe59f2feb
Chris Weber <chris@lookout.net>
2012-01-09 14:31:04 -0800

    Fixup reporting

--- web/ascii-unsafe/test.js
+++ web/ascii-unsafe/test.js
@@ -4,10 +4,14 @@ function getEncoding(alias) {
 	xhr.open('GET', "charset.php?alias=" + alias, false);
 	xhr.send();
 
-	numtests_complete++;
 	var teststatus = document.getElementById("teststatus");
-	teststatus.setAttribute("class","fail");
-	teststatus.innerHTML = "Testing " + numtests_complete + " of " + numtests;
+	if (numtests != -1)
+	{
+	  numtests_complete++;
+	  teststatus.setAttribute("class","fail");
+	  teststatus.innerHTML = "Testing " + numtests_complete + " of " + numtests \
+             + ": " + alias;
+	}
 
 
 	var result = document.createElement("span");
@@ -15,15 +19,6 @@ function getEncoding(alias) {
 	var resultFail = document.getElementById("resultFail");
 	var resultErr = document.getElementById("resultErr");
 	lbreak = document.createElement("br");
-
- 	// Does the alias exist?
-	//if(aliases.indexOf(alias) == -1) {
-	//  result.className = "fail";
-	//  result.innerHTML = alias + ",error: alias not found";
-	//  resultErr.appendChild(result);
-	//  resultErr.appendChild(lbreak);
-	//  return;
-	//}
 
 	try {
 	  var resp = xhr.responseText;
@@ -67,20 +62,40 @@ function getEncoding(alias) {
 	  resultErr.appendChild(lbreak);
 	}
 
-       var teststatus = document.getElementById("teststatus");
-        if (numtests == numtests_complete) {
+        if (numtests == numtests_complete && numtests != -1) {
           teststatus.setAttribute("class","pass");
           teststatus.innerHTML = "All tests complete...!";
         }
 
 }
+
+function clearResults() {
+  var resultPass = document.getElementById("resultPass");
+  var resultFail = document.getElementById("resultFail");
+  var resultErr = document.getElementById("resultErr");
+  // Clear the testzone
+  while (resultPass.hasChildNodes()) {
+    resultPass.removeChild(resultPass.lastChild);
+  }
+  while (resultFail.hasChildNodes()) {
+    resultFail.removeChild(resultFail.lastChild);
+  }
+  while (resultErr.hasChildNodes()) {
+    resultErr.removeChild(resultErr.lastChild);
+  }
+}
+
 // Loop through all of the aliases, testing each
 function testEncodings() {
+
+  numtests = aliases.length;
+  numtests_complete = 0;
+  clearResults();
   for (var i = 0; i < numtests; i++) {
 	var alias = aliases[i];
 	getEncoding(alias);
   }
 }
 
-var numtests = aliases.length;
+var numtests = -1;
 var numtests_complete = 0;
