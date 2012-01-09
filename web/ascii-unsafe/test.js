93939037fb1840e060851a27aaee3a2240155b5e
Chris Weber <chris@lookout.net>
2012-01-09 15:10:46 -0800

    Making XHR async to prevent UI lockups.

--- web/ascii-unsafe/test.js
+++ web/ascii-unsafe/test.js
@@ -1,72 +1,75 @@
 function getEncoding(alias) {
-	var xhr = new XMLHttpRequest();
-	// SYNCHRONOUS XHR required!
-	xhr.open('GET', "charset.php?alias=" + alias, false);
-	xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "charset.php?alias=" + alias, true);
 
-	var teststatus = document.getElementById("teststatus");
-	if (numtests != -1)
-	{
-	  numtests_complete++;
-	  teststatus.setAttribute("class","fail");
-	  teststatus.innerHTML = "Testing " + numtests_complete + " of " + numtests \
-             + ": " + alias;
-	}
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
 
+      var teststatus = document.getElementById("teststatus");
+      if (numtests != -1)
+      {
+        numtests_complete++;
+        teststatus.setAttribute("class","fail");
+        teststatus.innerHTML = "Testing " + numtests_complete + " of " + numtests + 
+          ": " + alias;
+      }
 
-	var result = document.createElement("span");
-	var resultPass = document.getElementById("resultPass");
-	var resultFail = document.getElementById("resultFail");
-	var resultErr = document.getElementById("resultErr");
-	lbreak = document.createElement("br");
+      var result = document.createElement("span");
+      var resultPass = document.getElementById("resultPass");
+      var resultFail = document.getElementById("resultFail");
+      var resultErr = document.getElementById("resultErr");
+      lbreak = document.createElement("br");
 
-	try {
-	  var resp = xhr.responseText;
+      try {
+        var resp = xhr.responseText;
 
-	  if (resp == " $%'()*+,-./<>:;=") {
-	    result.className = "pass";
-	    result.id = "pass";
-	    result.innerHTML = alias + ", pass";
-	    resultPass.appendChild(result);
-	    resultPass.appendChild(lbreak);
-	  }
-	  // Check for replacement chars in the respons,
-	  // which mean the conversion failed.
-	  else if (resp == "?" || resp == "!" || resp == "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") {
-	    result.className = "fail";
-	    result.id = "error";
-	    result.innerHTML = alias + ", conversion error";
-	    resultErr.appendChild(result);
-	    resultErr.appendChild(lbreak);
-	  }
-	  else if (resp == "not found") {
-	    result.className = "fail";
-	    result.id = "error";
-	    result.innerHTML = alias + ", not found";
-	    resultErr.appendChild(result);
-	    resultErr.appendChild(lbreak);
-	  }
-	  else {
-	    result.className = "fail";
-	    result.id = "fail";
-	    result.innerHTML = alias + ", fail";
-	    resultFail.appendChild(result);
-	    resultFail.appendChild(lbreak);
-	  }
-	}
-	catch(err) {
-	  result.className = "fail";
-	  result.id = "error";
-	  result.innerHTML = alias + ", error: " + err.description;
-	  resultErr.appendChild(result);
-	  resultErr.appendChild(lbreak);
-	}
-
-        if (numtests == numtests_complete && numtests != -1) {
-          teststatus.setAttribute("class","pass");
-          teststatus.innerHTML = "All tests complete...!";
+        if (resp == " $%'()*+,-./<>:;=") {
+          result.className = "pass";
+          result.id = "pass";
+          result.innerHTML = alias + ", pass";
+          resultPass.appendChild(result);
+          resultPass.appendChild(lbreak);
         }
+        // Check for replacement chars in the respons,
+        // which mean the conversion failed.
+        else if (resp == "?" || resp == "!" || resp == "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") {
+          result.className = "fail";
+          result.id = "error";
+          result.innerHTML = alias + ", conversion error";
+          resultErr.appendChild(result);
+          resultErr.appendChild(lbreak);
+        }
+        else if (resp == "not found") {
+          result.className = "fail";
+          result.id = "error";
+          result.innerHTML = alias + ", not found";
+          resultErr.appendChild(result);
+          resultErr.appendChild(lbreak);
+        }
+        else {
+          result.className = "fail";
+          result.id = "fail";
+          result.innerHTML = alias + ", fail";
+          resultFail.appendChild(result);
+          resultFail.appendChild(lbreak);
+        }
+      }
+      catch(err) {
+        result.className = "fail";
+        result.id = "error";
+        result.innerHTML = alias + ", error: " + err.description;
+        resultErr.appendChild(result);
+        resultErr.appendChild(lbreak);
+      }
 
+      if (numtests == numtests_complete && numtests != -1) {
+        teststatus.setAttribute("class","pass");
+        teststatus.innerHTML = "All tests complete...!";
+      }
+
+    }
+  }
+  xhr.send();
 }
 
 function clearResults() {
@@ -92,8 +95,8 @@ function testEncodings() {
   numtests_complete = 0;
   clearResults();
   for (var i = 0; i < numtests; i++) {
-	var alias = aliases[i];
-	getEncoding(alias);
+    var alias = aliases[i];
+    getEncoding(alias);
   }
 }
 
